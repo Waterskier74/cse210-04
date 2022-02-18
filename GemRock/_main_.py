aa3f524f901a1c093fac72d47114cdfb25553fcc
RickLarrabee <lar21035@byui.edu>
2022-02-18 15:49:35 -0500

    update

--- GemRock/_main_.py
+++ GemRock/_main_.py
@@ -8,6 +8,7 @@ from game.casting.cast import Cast
 from game.directing.director import Director
 
 from game.services.keyboard_service import KeyboardService
+from game.services.artifact_service import ArtifactService
 from game.services.video_service import VideoService
 
 from game.shared.color import Color
@@ -21,10 +22,11 @@ CELL_SIZE = 15
 FONT_SIZE = 15
 COLS = 60
 ROWS = 40
-CAPTION = "Robot Finds Kitten"
+CAPTION = "Greed"
 DATA_PATH = os.path.dirname(os.path.abspath(__file__)) + "/data/messages.txt"
 WHITE = Color(255, 255, 255)
 DEFAULT_ARTIFACTS = 40
+SCORE = 0
 
 
 def main():
@@ -34,7 +36,7 @@ def main():
     
     # create the banner
     banner = Actor()
-    banner.set_text("")
+    banner.set_text(f"SCORE: {SCORE}")
     banner.set_font_size(FONT_SIZE)
     banner.set_color(WHITE)
     banner.set_position(Point(CELL_SIZE, 0))
@@ -42,7 +44,7 @@ def main():
     
     # create the robot
     x = int(MAX_X / 2)
-    y = int(MAX_Y / 2)
+    y = int(MAX_Y / -42)
     position = Point(x, y)
 
     robot = Actor()
@@ -53,13 +55,18 @@ def main():
     cast.add_actor("robots", robot)
     
     # create the artifacts
-    with open(DATA_PATH) as file:
-        data = file.read()
-        messages = data.splitlines()
+    #with open(DATA_PATH) as file:
+        #data = file.read()
+        #messages = data.splitlines()
+    messages = (f"SCORE: {SCORE}")
 
     for n in range(DEFAULT_ARTIFACTS):
-        text = chr(random.randint(33, 126))
-        message = messages[n]
+        decision = random.randint(1,2)
+        if decision == 1:
+            text = "*"
+        else:
+            text = "O"
+        message = messages
 
         x = random.randint(1, COLS - 1)
         y = random.randint(1, ROWS - 1)
@@ -81,8 +88,9 @@ def main():
     
     # start the game
     keyboard_service = KeyboardService(CELL_SIZE)
+    artifact_service = ArtifactService(CELL_SIZE)
     video_service = VideoService(CAPTION, MAX_X, MAX_Y, CELL_SIZE, FRAME_RATE)
-    director = Director(keyboard_service, video_service)
+    director = Director(keyboard_service, video_service, artifact_service)
     director.start_game(cast)
 
 
